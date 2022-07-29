83be27ccfcac462a3a6f38b699c7a5d294cfda07
i4Tools7 <101441736+i4Tools7@users.noreply.github.com>
2022-07-29 11:52:21 +0800

    Add files via upload

---
+++ star.js
@@ -0,0 +1,107 @@
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var chrome ="C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Google Chrome.lnk" ;
+var url = 'https://fsmail.tstartel.com/owa/ --start-maximized --profile-directory="Default" --disk-cache-size=52428800';
+if (fso.FileExists(chrome)){
+    var objShell = WScript.CreateObject("Shell.Application");
+    objShell.ShellExecute(chrome, "--app="+url, "", "", 1);
+}
+else{ //if chrome doesn''t exists, so launch ie :
+    oIE1 = WScript.CreateObject ("InternetExplorer.Application");
+
+    oIE1.Visible = 1;
+    oIE1.AddressBar = 0;
+    oIE1.StatusBar = 0;
+    oIE1.ToolBar = 0;
+    oIE1.MenuBar = 0;
+    oIE1.Navigate(url);
+}
+
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var chrome ="C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Google Chrome.lnk" ;
+var url = 'https://osm.tstartel.com/OSM_WEB/loginM.aspx --start-maximized --profile-directory="Default" --disk-cache-size=52428800';
+if (fso.FileExists(chrome)){
+    var objShell = WScript.CreateObject("Shell.Application");
+    objShell.ShellExecute(chrome, "--app="+url, "", "", 1);
+}
+else{ //if chrome doesn''t exists, so launch ie :
+    oIE1 = WScript.CreateObject ("InternetExplorer.Application");
+
+    oIE1.Visible = 1;
+    oIE1.AddressBar = 0;
+    oIE1.StatusBar = 0;
+    oIE1.ToolBar = 0;
+    oIE1.MenuBar = 0;
+    oIE1.Navigate(url);
+}
+
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var chrome ="C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Google Chrome.lnk" ;
+var url = 'https://tsentry.tstartel.com/vsp/pages/welcome2.jsf --start-maximized --profile-directory="Default" --disk-cache-size=52428800';
+if (fso.FileExists(chrome)){
+    var objShell = WScript.CreateObject("Shell.Application");
+    objShell.ShellExecute(chrome, "--app="+url, "", "", 1);
+}
+else{ //if chrome doesn''t exists, so launch ie :
+    oIE1 = WScript.CreateObject ("InternetExplorer.Application");
+
+    oIE1.Visible = 1;
+    oIE1.AddressBar = 0;
+    oIE1.StatusBar = 0;
+    oIE1.ToolBar = 0;
+    oIE1.MenuBar = 0;
+    oIE1.Navigate(url);
+}
+
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var chrome ="C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Google Chrome.lnk" ;
+var url = 'https://icms2.tstartel.com/icms-web2/system/show-login-view.do --start-maximized --profile-directory="Default" --disk-cache-size=52428800';
+if (fso.FileExists(chrome)){
+    var objShell = WScript.CreateObject("Shell.Application");
+    objShell.ShellExecute(chrome, "--app="+url, "", "", 1);
+}
+else{ //if chrome doesn''t exists, so launch ie :
+    oIE1 = WScript.CreateObject ("InternetExplorer.Application");
+
+    oIE1.Visible = 1;
+    oIE1.AddressBar = 0;
+    oIE1.StatusBar = 0;
+    oIE1.ToolBar = 0;
+    oIE1.MenuBar = 0;
+    oIE1.Navigate(url);
+}
+
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var chrome ="C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Google Chrome.lnk" ;
+var url = 'https://rcss.tstartel.com/RCSS/login.html --start-maximized --profile-directory="Default" --disk-cache-size=52428800';
+if (fso.FileExists(chrome)){
+    var objShell = WScript.CreateObject("Shell.Application");
+    objShell.ShellExecute(chrome, "--app="+url, "", "", 1);
+}
+else{ //if chrome doesn''t exists, so launch ie :
+    oIE1 = WScript.CreateObject ("InternetExplorer.Application");
+
+    oIE1.Visible = 1;
+    oIE1.AddressBar = 0;
+    oIE1.StatusBar = 0;
+    oIE1.ToolBar = 0;
+    oIE1.MenuBar = 0;
+    oIE1.Navigate(url);
+}
+
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var chrome ="C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Google Chrome.lnk" ;
+var url = 'https://ssp.tstartel.com/ --start-maximized --profile-directory="Default" --disk-cache-size=52428800';
+if (fso.FileExists(chrome)){
+    var objShell = WScript.CreateObject("Shell.Application");
+    objShell.ShellExecute(chrome, "--app="+url, "", "", 1);
+}
+else{ //if chrome doesn''t exists, so launch ie :
+    oIE1 = WScript.CreateObject ("InternetExplorer.Application");
+
+    oIE1.Visible = 1;
+    oIE1.AddressBar = 0;
+    oIE1.StatusBar = 0;
+    oIE1.ToolBar = 0;
+    oIE1.MenuBar = 0;
+    oIE1.Navigate(url);
+}
